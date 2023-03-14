79881253a810ea3b6ade2486021f196e88c4c852
Miguel <99726342+mig-code@users.noreply.github.com>
2023-03-14 18:51:26 +0100

    :rotating_light: fix audit indent

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
-   
-
     return (
         <div className="App">
             <header className="App-header">
