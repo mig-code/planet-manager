ca36c395ae132ca0861c237f2be8fea2279bcc39
Miguel <99726342+mig-code@users.noreply.github.com>
2023-03-14 18:34:36 +0100

    :construction_worker: add sonar config

--- src/index.tsx
+++ src/index.tsx
@@ -6,15 +6,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 
-const container = document.getElementById('root')!;
+const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
