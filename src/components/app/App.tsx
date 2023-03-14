1fcebcb2723f026ff925bef0b2be4427bb05840a
Miguel <99726342+mig-code@users.noreply.github.com>
2023-03-14 20:15:18 +0100

    :construction: update public folder files

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -7,6 +7,10 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <h1>Planet Manager</h1>
+                <p>
+                    The ultimate Star Wars app for managing your own planet,
+                    building infrastructure, and defending against invaders.
+                </p>
             </header>
         </div>
     );
