c9a819ab1582ef08092ca50ded9094dcedfb0cd2
Miguel <99726342+mig-code@users.noreply.github.com>
2023-03-15 16:33:48 +0100

    :construction: add plantes api service

---
+++ src/services/planets.api.ts
@@ -0,0 +1,37 @@
+import { planetsAdapter } from './../adapters/planet.adapter';
+
+const url = process.env.REACT_APP_PLANETS_API_URL as string;
+
+const allPlanetsQuery = `query {
+  allPlanets {
+    planets {
+      population
+      terrains
+      id
+      diameter
+      climates
+      name
+      residentConnection {
+        residents {
+          name
+        }
+      }
+    }
+  }
+}`;
+
+export const getAllPlanets = async () => {
+    try {
+        const resp = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ query: allPlanetsQuery }),
+        });
+        const result = await resp.json();
+        return planetsAdapter(result);
+    } catch (error) {
+        console.log(error);
+    }
+};
