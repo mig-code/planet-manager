3b74812aab3c269c28c406f6aa3bd6feb1385898
Miguel <99726342+mig-code@users.noreply.github.com>
2023-03-15 16:27:03 +0100

    :label: Add planet type

---
+++ src/types/planet.ts
@@ -0,0 +1,9 @@
+export type PlanetInfo = {
+    id: string;
+    name: string;
+    population: string;
+    terrains: Array<string>;
+    climates: Array<string>;
+    diameter: number;
+    residents: Array<string>;
+};
