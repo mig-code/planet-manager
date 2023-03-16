385d236199c578cf40712d3e310b757b47659e6f
Miguel <99726342+mig-code@users.noreply.github.com>
2023-03-16 09:24:14 +0100

    :white_check_mark: update test mocking api respons

--- src/pages/home/home.page.test.tsx
+++ src/pages/home/home.page.test.tsx
@@ -3,15 +3,14 @@ import { render, screen } from '@testing-library/react';
 import { HomePage } from './home.page';
 import { Provider } from 'react-redux';
 import { store } from '../../store/store';
+import { mockPlanets } from '../../mocks/planets.mocks';
+import { getAllPlanets } from '../../services/planets.api';
 
-jest.mock('../../components/planets.list/planets.list', () => {
-    return {
-        PlanetsList: () => {
-            return <div>PlanetsList</div>;
-        },
-    };
-});
+// mock the getAllPlanets function
 
+jest.mock('../../services/planets.api', () => ({
+    getAllPlanets: jest.fn(),
+}));
 describe('When render Home page component', () => {
     test('It should render the PlanetsList component', () => {
         render(
@@ -20,7 +19,18 @@ describe('When render Home page component', () => {
             </Provider>
         );
 
-        const planetsListMock = screen.getByText('PlanetsList');
+        const planetsListMock = screen.getByText('Planets List');
+        expect(planetsListMock).toBeInTheDocument();
+    });
+    test('When there are planets it should update the store', () => {
+        (getAllPlanets as jest.Mock).mockResolvedValueOnce(mockPlanets);
+        render(
+            <Provider store={store}>
+                <HomePage></HomePage>
+            </Provider>
+        );
+
+        const planetsListMock = screen.getByText('Planets List');
         expect(planetsListMock).toBeInTheDocument();
     });
 });
