9e0c71c1fa900ccbd76bbba1deb1ec77c6c07c9b
Miguel <99726342+mig-code@users.noreply.github.com>
2023-03-16 11:09:43 +0100

    :white_check_mark: update and refactor tests

--- src/components/planets.list/planets.list.test.tsx
+++ src/components/planets.list/planets.list.test.tsx
@@ -2,17 +2,20 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { PlanetsList } from './planets.list';
 import { Provider } from 'react-redux';
-import { store } from '../../store/store';
+
+import { mockPlanetsStore } from '../../mocks/store.mock';
 
 describe('When render PlanetsList component', () => {
-    test('It should render the title', () => {
+    test('It should render the list of planets', () => {
+        const mockStore = mockPlanetsStore;
+
         render(
-            <Provider store={store}>
+            <Provider store={mockStore}>
                 <PlanetsList></PlanetsList>
             </Provider>
         );
 
-        const title = screen.getByRole('heading', { name: 'Planets List' });
-        expect(title).toBeInTheDocument();
+        const mockPlanetName = screen.getByText('Tatooine');
+        expect(mockPlanetName).toBeInTheDocument();
     });
 });
--- src/pages/home/home.page.test.tsx
+++ src/pages/home/home.page.test.tsx
@@ -3,34 +3,25 @@ import { render, screen } from '@testing-library/react';
 import { HomePage } from './home.page';
 import { Provider } from 'react-redux';
 import { store } from '../../store/store';
-import { mockPlanets } from '../../mocks/planets.mocks';
-import { getAllPlanets } from '../../services/planets.api';
+import { usePlanets } from '../../hooks/usePlanets';
 
-// mock the getAllPlanets function
+jest.mock('../../hooks/usePlanets');
 
-jest.mock('../../services/planets.api', () => ({
-    getAllPlanets: jest.fn(),
-}));
 describe('When render Home page component', () => {
     test('It should render the PlanetsList component', () => {
+        const mockHandleLoadAllPlanets = jest.fn();
+        (usePlanets as jest.Mock).mockReturnValue({
+            handleLoadAllPlanets: mockHandleLoadAllPlanets,
+        });
         render(
             <Provider store={store}>
                 <HomePage></HomePage>
             </Provider>
         );
 
-        const planetsListMock = screen.getByText('Planets List');
-        expect(planetsListMock).toBeInTheDocument();
-    });
-    test('When there are planets it should update the store', () => {
-        (getAllPlanets as jest.Mock).mockResolvedValueOnce(mockPlanets);
-        render(
-            <Provider store={store}>
-                <HomePage></HomePage>
-            </Provider>
-        );
+        const planetsListTitle = screen.getByText('Planets List');
+        expect(planetsListTitle).toBeInTheDocument();
 
-        const planetsListMock = screen.getByText('Planets List');
-        expect(planetsListMock).toBeInTheDocument();
+        expect(mockHandleLoadAllPlanets).toHaveBeenCalled();
     });
 });
