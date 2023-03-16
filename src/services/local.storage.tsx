c9f063862d015d00f778c2dd2bcaecc9a956a3b8
Miguel <99726342+mig-code@users.noreply.github.com>
2023-03-16 12:53:37 +0100

    :construction: add local.storage service

---
+++ src/services/local.storage.tsx
@@ -0,0 +1,12 @@
+export const getDataLocalStorage = <T,>(storeName: string): Array<T> => {
+    const result = localStorage.getItem(storeName);
+    if (!result) return [];
+    return JSON.parse(result);
+};
+
+export const persistDataLocalStorage = <T,>(
+    storeName: string,
+    data: Array<T>
+): void => {
+    localStorage.setItem(storeName, JSON.stringify(data));
+};
